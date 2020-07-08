026e4c96c07501db1a7f9c289512379354ea3914
Kat Leight <leightkt@gmail.com>
2020-07-08 21:33:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   lowest_value_key = nil
   name_hash.each do |key, value|
-    if lowest_value = nil
+    if lowest_value == nil
       lowest_value = value
       lowest_value_key = key
     elsif value < lowest_value
